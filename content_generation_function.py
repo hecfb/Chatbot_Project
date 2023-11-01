2c10eccf5cb5e30d9730629320a1309fd6c0ffe1
hecfb <148881381+hecfb@users.noreply.github.com>
2023-11-01 16:04:31 -0400

    v3
    added together the py functions and the html since the code wasn't extensive enough

--- content_generation_function.py
+++ content_generation_function.py
@@ -10,47 +10,72 @@ logger.setLevel(logging.INFO)
 
 # Initialize clients
 s3 = boto3.client('s3')
-openai.api_key = "API KEY"
+openai.api_key = 'api key'
 
 
-def generate_content(prompt):
+def fetch_content_from_s3(bucket, key):
     try:
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            max_tokens=150
-        )
-        return response["choices"][0]["text"].strip()
-    except Exception as e:
-        logger.error(f"OpenAI error: {e}")
+        response = s3.get_object(Bucket=bucket, Key=key)
+        return response['Body'].read().decode()
+    except (BotoCoreError, ClientError) as e:
+        logger.error(f"S3 error: {e}")
         raise
 
 
 def save_to_s3(content, bucket, key):
     try:
-        s3.put_object(
-            Bucket=bucket,
-            Key=key,
-            Body=content
-        )
+        s3.put_object(Bucket=bucket, Key=key, Body=content)
+    except (BotoCoreError, ClientError) as e:
+        logger.error(f"S3 error: {e}")
+        raise
+
+
+def manage_content(content):
+    inappropriate_words = ["example_bad_word1", "example_bad_word2"]
+    for word in inappropriate_words:
+        content = content.replace(word, "[REDACTED]")
+    return content
+
+
+def publish_content(source_key, destination_key, bucket):
+    try:
+        s3.copy_object(
+            Bucket=bucket, CopySource=f"{bucket}/{source_key}", Key=destination_key)
     except (BotoCoreError, ClientError) as e:
         logger.error(f"S3 error: {e}")
         raise
 
 
 def lambda_handler(event, context):
-    user_input = event.get("input", "Default prompt if none provided")
-    try:
-        generated_content = generate_content(user_input)
-        save_to_s3(generated_content, "chatgpt-test-bucket",
-                   "generated-content.txt")
-    except Exception as e:
-        return {
-            "statusCode": 500,
-            "error": str(e)
-        }
+    messages = event.get('messages', [])
+    role = messages[-1]['role'] if messages else None
+    content = messages[-1]['content'] if messages else None
+
+    if role == "user" and content:
+        # Generate Content
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+        )
+        generated_content = response['choices'][0]['message']['content']
+        save_to_s3(generated_content, 'chatgpt-test-bucket',
+                   'generated-content.txt')
+
+        # Manage Content
+        managed_content = manage_content(generated_content)
+        save_to_s3(managed_content, 'chatgpt-test-bucket',
+                   'filtered-content.txt')
+
+        # Publish Content
+        publish_content('filtered-content.txt',
+                        'published/filtered-content.txt', 'chatgpt-test-bucket')
+
+        messages.append({"role": "assistant", "content": managed_content})
+    else:
+        messages.append(
+            {"role": "assistant", "content": "I couldn't process your request. Please try again."})
 
     return {
-        "statusCode": 200,
-        "content": generated_content
+        'statusCode': 200,
+        'messages': messages
     }
